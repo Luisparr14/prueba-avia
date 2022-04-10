cb5f44a8753d66c77f0b25a4fde91b2fd46d9e5b
Luis Angel <luisparr14@gmail.com>
2022-04-09 22:57:23 -0500

    Using esbuild for minify

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const favicon = require('serve-favicon');
 const logger = require('morgan');
 const routes = require('./routes');
 const cors = require('cors');
@@ -10,15 +9,16 @@ const corsOptions = {
   origin: '*',
 };
 
-if(process.env.NODE_ENV === 'development') {
+if (process.env.NODE_ENV === 'development') {
   app.use(logger('dev'));
 }
+const url = process.env.NODE_ENV === 'production' ? 'public/prueba-avia' : 'build/public/prueba-avia';
+
 app.set('port', process.env.PORT || 3000)
-app.use(favicon(path.join(__dirname, 'build/prueba-avia', 'favicon.ico')));
-app.use('/api/v1',cors(corsOptions) ,routes);
-app.use('/', express.static(path.join(__dirname, 'build/prueba-avia')));
+app.use('/api/v1', cors(corsOptions), routes);
+app.use('/', express.static(path.join(__dirname, url)));
 app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, url, 'index.html'));
   res.send('<script>window.location.href = "/"</script>');
-  res.sendFile(path.join(__dirname, 'build/prueba-avia/index.html'));
 });
 module.exports = app
